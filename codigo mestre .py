6ec6f67701ac1a2bc7abd01963517297ed7b3957
hfelix94 <hfelix94@hotmail.com>
2015-06-24 18:41:44 -0300

    paginas funcionando

--- codigo mestre .py
+++ codigo mestre .py
@@ -1,5 +1,4 @@
-from cadastro_gerado import *
-from login_gerado import *
+
 
 import sys
 from PyQt4.QtGui import *
@@ -10,21 +9,21 @@ class Aplicativo(QWidget):
         super(Aplicativo, self).__init__()
         self.pushButton = QPushButton('teste')
         self.pushButton.clicked.connect(self.teste)
-        self.janela1 = QWidget()
-        layout_1 = QVBoxLayout(self.janela1)
+        self.pag_A = QWidget()
+        layout_1 = QVBoxLayout(self.pag_A)
         layout_1.addWidget(QLabel('janela1'))
         layout_1.addWidget(self.pushButton)
-        self.janela2 = QWidget()
-        layout_2 = QVBoxLayout(self.janela2)
+        self.pag_B = QWidget()
+        layout_2 = QVBoxLayout(self.pag_B)
         layout_2.addWidget(QLabel('janela2'))
         layout_main = QGridLayout(self)
-        layout_main.addWidget(self.janela1, 0,0,1,1)
-        layout_main.addWidget(self.janela2, 0,0,1,1)
-        self.janela2.hide()
+        layout_main.addWidget(self.pag_A, 0,0,1,1)
+        layout_main.addWidget(self.pag_B, 0,0,1,1)
+        self.pag_B.hide()
 
     def teste(self):
-        self.janela1.hide()
-        self.janela2.show()
+        self.pag_A.hide()
+        self.pag_B.show()
 
 
 
@@ -40,7 +39,7 @@ class Aplicativo(QWidget):
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = Aplicativo()
-   ex.show()
-   sys.exit(app.exec_()
+    app = QApplication(sys.argv)
+    ex = Aplicativo()
+    ex.show()
+    sys.exit(app.exec_())
